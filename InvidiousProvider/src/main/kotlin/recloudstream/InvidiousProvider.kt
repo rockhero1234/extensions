35bb0701f9f2bc3de76a5a5e36abed192a29798e
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-10-12 19:49:19 +0530

    Update InvidiousProvider.kt

--- InvidiousProvider/src/main/kotlin/recloudstream/InvidiousProvider.kt
+++ InvidiousProvider/src/main/kotlin/recloudstream/InvidiousProvider.kt
@@ -14,52 +14,56 @@ class InvidiousProvider : MainAPI() {
     override var lang = "en"
     override val hasMainPage = true
 
+    
+    fun getData(titled: String): List<MovieSearchResponse> {
+    val response = app.post(
+        "$mainUrl/wp-admin/admin-ajax.php",
+        data = mapOf(
+            "filters[search]" to "",
+            "filters[recommend]" to "false",
+            "filters[date-from]" to "",
+            "filters[date-to]" to "",
+            "filters[mode]" to "streaming-soon",
+            "filters[page]" to "0",
+            "action" to "mi_events_load_data",
+            "mode" to "$titled",
+            "start" to "0",
+            "length" to "20",
+            "customcatalog" to "0"
+        ),
+        headers = mapOf(
+            "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8",
+            "Accept" to "*/*",
+            "X-Requested-With" to "XMLHttpRequest",
+            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            "Referer" to "$mainUrl"
+        )
+    ).text
+
+    val json = tryParseJson<Map<String, Any>>(response)
+    val dataList = json?.get("data") as? List<Map<String, Any>>
+
+    val movies = dataList?.map { entry ->
+        newMovieSearchResponse(
+            name = entry["title"].toString(),
+            url = entry["link"].toString(),
+            type = TvType.Movie
+        ) {
+            this.posterUrl = entry["image"].toString()
+            //this.plot = entry["review"].toString()
+        }
+    } ?: emptyList()
+
+    return movies
+    }
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-       
-        val response = app.post(
-            "$mainUrl/wp-admin/admin-ajax.php",
-            data = mapOf(
-                "filters[search]" to "",
-                "filters[recommend]" to "false",
-                "filters[date-from]" to "",
-                "filters[date-to]" to "",
-                "filters[mode]" to "streaming-soon",
-                "filters[page]" to "0",
-                "action" to "mi_events_load_data",
-                "mode" to "streaming-soon",
-                "start" to "0",
-                "length" to "20",
-                "customcatalog" to "0"
-            ),
-            headers = mapOf(
-                "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8",
-                "Accept" to "*/*",
-                "X-Requested-With" to "XMLHttpRequest",
-                "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-                "Referer" to "$mainUrl"
-            )
-        ).text
-        
-        val json = tryParseJson<Map<String, Any>>(response)
-        val dataList = json?.get("data") as? List<Map<String, Any>>
-        
-        // Debugging output
-        println("Response JSON: $json")
-        
-        val movies = dataList?.map { entry ->
-            newMovieSearchResponse(
-                name = entry["title"].toString(),
-                url = entry["link"].toString(),
-                type = TvType.Movie
-            ) {
-                this.posterUrl = entry["image"].toString()
-                //this.plot = entry["review"].toString()
-            }
-        } ?: emptyList()
-        
+        val stsoon = getData("streaming-soon")
+        val stnow = getData("streaming-now")
         return newHomePageResponse(
             listOf(
-                HomePageList("Streaming Soon", movies, false)
+                HomePageList("Streaming Soon", stsoon, false),
+                HomePageList("Streaming Now", stnow, false),
+                
             ), true
         )
     }
