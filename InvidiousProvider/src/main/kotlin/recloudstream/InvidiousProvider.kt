a8a0dfca215d52b6be683fe08401a27657681c0d
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-10-12 20:06:38 +0530

    Update InvidiousProvider.kt

--- InvidiousProvider/src/main/kotlin/recloudstream/InvidiousProvider.kt
+++ InvidiousProvider/src/main/kotlin/recloudstream/InvidiousProvider.kt
@@ -49,7 +49,7 @@ class InvidiousProvider : MainAPI() {
             url = entry["link"].toString(),
             type = TvType.Movie
         ) {
-            this.posterUrl = entry["image"].toString()
+            this.posterUrl = entry["big-image"].toString()
             //this.plot = entry["review"].toString()
         }
     } ?: emptyList()
@@ -82,6 +82,7 @@ class InvidiousProvider : MainAPI() {
 val tags = listOf(
     doc.select("span.single-mevents-platforms-row-date").text().toString(),
     doc.select("span.rating-span").first().text().toString(),
+    doc.select('img.single-mevents-platforms-row-image').attr("alt").toString(),
     if (dtsplit.size > 1) dtsplit[1] else "",
     if (dtsplit.size > 2) dtsplit[2] else "",
     if (dtsplit.size > 3) dtsplit[3] else ""
