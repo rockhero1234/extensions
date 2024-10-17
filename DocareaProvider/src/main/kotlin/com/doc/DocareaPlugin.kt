ccc1b036a30f66efe3d287e17adbfa8ccc2cbb04
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-10-17 15:17:37 +0530

    Update DocareaPlugin.kt

--- DocareaProvider/src/main/kotlin/com/doc/DocareaPlugin.kt
+++ DocareaProvider/src/main/kotlin/com/doc/DocareaPlugin.kt
@@ -1,1 +1,14 @@
 
+package com.doc
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class TopdocumentaryfilmsPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(DocareaProvider())
+    }
+}
